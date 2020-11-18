e404f105f95db7977ba17bff8af96fe4ae5cf511
oichii <i.hawk@yandex.ru>
2020-11-18 14:41:53 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <thread>
+#include <chrono>
+#include <cmath>
+#include <omp.h>
+#include <stdio.h>
+
+using namespace std;
+using namespace std::chrono;
+
+string* result;
+
+vector<string> readStrings(const char* path) {
+    ifstream in(path);
+    vector<string> result(0);
+    string line;
+    while (in >> line)
+        result.push_back(line);
+    in.close();
+    return result;
+}
+
+string getCyph(char c, vector<string> keys) {
+    return keys[c - 'a'];
+}
+
+void encryptWord(string word, vector<string> keys, int i) {
+    string en = "";
+    for (int i = 0; i < word.size(); i++)
+        en += getCyph(word[i], keys);
+    result[i] = en;
+}
+
+void threadEncrypt(string text, int numOfThreads, vector<string> keys) {
+    auto startTime = high_resolution_clock::now();
+
+    // main loop
+    result = new string[numOfThreads];
+    int wordLength = ceil(text.size() * 1.0 / numOfThreads);
+
+    string* words = new string[numOfThreads];
+    int begin = 0;
+    int i = 0;
+    
+    for (int i = 0; i < numOfThreads; i++) {
+        words[i] = text.substr(begin, wordLength);
+        begin += wordLength;
+    }
+
+    #pragma omp parallel for
+    for (int i = 0; i < numOfThreads; i++)
+        encryptWord(words[i], keys, i);
+
+    delete[] words;
+
+    cout << "Encrypted message: ";
+    for (int i = 0; i < numOfThreads; i++)
+        cout << result[i];
+    cout << endl;
+
+    auto stopTime = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stopTime - startTime);
+    cout << "Time spent on parallel calculation (number of threads - " << numOfThreads << ") : " << duration.count() * 1.0 / 1000000 << endl << endl;
+}
+
+
+int main(int argc, char* argv[]) {
+    // argv[1] - input file
+    // argv[2] - keys file
+    if (argc < 3) {
+        cout << "Not enough console arguments" << endl;
+        cin.get();
+        return 0;
+    }
+    if (argc > 3) {
+        cout << "Too many console arguments" << endl;
+        cin.get();
+        return 0;
+    }
+    auto keys = readStrings(argv[2]);
+    auto strings = readStrings(argv[1]);
+    string text = "";
+    for (string s : strings)
+        text += s;
+    cout << "Initial message: " << text << endl;
+    for (int i = 0; i < sqrt(text.size()); i++)
+        threadEncrypt(text, i + 1, keys);
+    cin.get();
+    delete[] result;
+    return 0;
+}
